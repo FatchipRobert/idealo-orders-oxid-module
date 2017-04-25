7d45e8623be30c358d659a9417d6bf5dcb29736d
Robert Müller <robert.mueller@fatchip.de>
2017-04-25 17:25:09 +0200

    Fixed encoding problem in admin config

--- copy_this/modules/fcIdealo/extend/application/controllers/admin/fcidealo_module_config.php
+++ copy_this/modules/fcIdealo/extend/application/controllers/admin/fcidealo_module_config.php
@@ -21,7 +21,7 @@ class fcidealo_module_config extends fcidealo_module_config_parent
 
     protected $_aDefaultIdealoPaymentTypes = array(
         'CREDITCARD' => 'Credit Card Payment Method (Heidelpay)',
-        'SOFORT' => 'SOFORT Überweisung Payment Method',
+        'SOFORT' => 'SOFORT &Uuml;berweisung Payment Method',
         'PAYPAL' => 'PayPal Payment Method',
     );
     
